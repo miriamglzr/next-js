7d859cc82fe7e011307bb33a5aa5e224458584ff
miriam <miriam_glzr@hotmail.com>
2022-05-04 22:25:42 +0200

    api post connected inside context

--- context/Tasks.tsx
+++ context/Tasks.tsx
@@ -1,4 +1,6 @@
+import axios from "axios";
 import { createContext, ReactNode, useContext, useState } from "react";
+import useSWR from "swr";
 
 export type Task = {
 	id: number;
@@ -14,9 +16,20 @@ const AppCtx = createContext<AppContextInterface | null>(null);
 
 export const AppCtxProvider = ({ children }: { children: ReactNode }) => {
 	const [tasks, setTasks] = useState<Task[]>([]);
-	const onTaskCreated = (task: Task) => {
+
+	const onTaskCreated = async (task: Task) => {
 		setTasks([...tasks, task]);
+
+		const url = `/api/tasks`;
+		await axios.post(url, task);
 	};
+
+	const fetcher = (url: string) => axios.get(url);
+
+	const url = `/api/tasks`;
+
+	const { data } = useSWR(url, fetcher);
+	console.log(data?.data);
 
 	return (
 		<AppCtx.Provider value={{ tasks, onTaskCreated }}>
--- pages/api/tasks.ts
+++ pages/api/tasks.ts
@@ -1,22 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import type { Task } from '../../context/Tasks'
+import type { NextApiRequest, NextApiResponse } from "next";
+import type { Task } from "../../context/Tasks";
 
 declare global {
-var items: Task[]
+	var items: Task[];
 }
 
-globalThis.items = [{id: 1, name: 'Do homework' }]
-// do a function to add into the array
+globalThis.items = [{ id: 1, name: "Do homework" }];
+
 
 export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Task[]>, item?: Task
+	req: NextApiRequest,
+	res: NextApiResponse<Task[]>
 ) {
-    if (req.method === 'POST' && item) {
-          // Process a POST request
-        globalThis.items.push(item)
-        res.status(201).json(items)
-    }  else {
-  res.status(200).json(items)}
+	if (req.method === "POST" && req.body) {
+		globalThis.items.push(req.body);
+		res.status(201).json(items);
+	} else {
+		res.status(200).json(items);
+	}
 }
